661292119dc2b8c205b9354af525e907319415e7
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-09-07 11:50:41 -0400

    Finished up the audio controls component
    I finished up the audio controls component using hooks

--- src/audioControlsA.js
+++ src/audioControlsA.js
@@ -1,16 +1,61 @@
 import React, { useState } from "react";
 
 export default function AudioControls() {
-  var [volume, setVolume] = useState(50);
+  var [volume, setVolume] = useState(85);
+  var [treble, setTreble] = useState(33);
+  var [mid, setMid] = useState(71);
+  var [bass, setBass] = useState(92);
 
   return (
     <span>
       <div>
-        <button style={buttonStyle}>
+        <button
+          style={buttonStyle}
+          onClick={() => setVolume((volume = volume + 1))}
+        >
           <h3>+</h3>
         </button>
         {volume} Volume
-        <button style={buttonStyle}>
+        <button
+          style={buttonStyle}
+          onClick={() => setVolume((volume = volume - 1))}
+        >
+          <h3>-</h3>
+        </button>
+      </div>
+
+      <div>
+        <button
+          style={buttonStyle}
+          onClick={() => setTreble((treble = treble + 1))}
+        >
+          <h3>+</h3>
+        </button>
+        {treble} Treble
+        <button
+          style={buttonStyle}
+          onClick={() => setTreble((treble = treble - 1))}
+        >
+          <h3>-</h3>
+        </button>
+      </div>
+
+      <div>
+        <button style={buttonStyle} onClick={() => setMid((mid = mid + 1))}>
+          <h3>+</h3>
+        </button>
+        {mid} Mid
+        <button style={buttonStyle} onClick={() => setMid((mid = mid - 1))}>
+          <h3>-</h3>
+        </button>
+      </div>
+
+      <div>
+        <button style={buttonStyle} onClick={() => setBass((bass = bass + 1))}>
+          <h3>+</h3>
+        </button>
+        {bass} Bass
+        <button style={buttonStyle} onClick={() => setBass((bass = bass - 1))}>
           <h3>-</h3>
         </button>
       </div>
