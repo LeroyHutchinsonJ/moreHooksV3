d98ace14026241cdc1869c4e893abc7598e0a406
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-09-21 16:38:34 -0400

    Created Pizza Form
    Created a little pizza form that uses hooks

--- src/radioButtons.js
+++ src/radioButtons.js
@@ -2,31 +2,69 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 
 var RadioButtonExample = () => {
-  var [letter, setLetter] = useState("");
-
-  console.log(letter);
+  var [sizeLetter, setLetter] = useState("");
+  var [glutenValue, setGlutenValue] = useState(false);
 
   return (
-    <form>
-      <input
-        type="radio"
-        value="a"
-        checked={letter === "a"}
-        onChange={() => setLetter("a")}
-      />
-      <input
-        type="radio"
-        value="b"
-        checked={letter === "b"}
-        onChange={() => setLetter("b")}
-      />
-      <input
-        type="radio"
-        value="c"
-        checked={letter === "c"}
-        onChange={() => setLetter("c")}
-      />
-      <div>Blah</div>
+    <form className="pizzaForm">
+      <h1>Order Your Pizza</h1>
+      <h3>Size</h3>
+      <div className="pizzaSizes">
+        <div>
+          <input
+            type="radio"
+            checked={sizeLetter === "a"}
+            onChange={() => setLetter("a")}
+          />
+          Small
+        </div>
+        <div>
+          <input
+            type="radio"
+            checked={sizeLetter === "b"}
+            onChange={() => setLetter("b")}
+          />
+          Medium
+        </div>
+        <div>
+          <input
+            type="radio"
+            checked={sizeLetter === "c"}
+            onChange={() => setLetter("c")}
+          />
+          Large
+        </div>
+      </div>
+      <br />
+      <div>
+        <div>Topping</div>
+        <select>
+          <option>Pepparoni</option>
+          <option>Sausages</option>
+          <option>Olive</option>
+          <option>Garlic</option>
+          <option>Cheesey Crumbs</option>
+          <option>Poke sauce</option>
+          <option>Bread Crumbs</option>
+        </select>
+      </div>
+      <br />
+      <div>
+        <input
+          type="radio"
+          checked={glutenValue === true}
+          onClick={() => setGlutenValue(!glutenValue)}
+        />
+        Gluten Free
+      </div>
+      <br />
+
+      <div>
+        <div>Special Instructions</div>
+        <input />
+      </div>
+      <br />
+      <button>Send Order</button>
     </form>
   );
 };
